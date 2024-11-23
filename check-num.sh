ce397db94148360c02bbad728b39e6ed025db81d
dhanushboga <dhanushboga555@gmail.com>
2024-11-23 16:56:01 +0530

    check-num

---
+++ check-num.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+read -p "Enter the number : " num
+
+
+if [[ $num -gt 0 ]]
+then
+    echo "it is positive number";
+elif [[ $num -lt 0 ]]
+then
+    echo "it is negative number";
+else
+    echo "it is zero";
+fi
