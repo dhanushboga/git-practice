4081b5b6e2bacb5923e23fd834c917e815a82714
dhanushboga <dhanushboga555@gmail.com>
2024-11-23 18:18:04 +0530

    Script using variables

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+person=$1
+person1=$2
+echo "Hi $person how are you"
+echo "Hi $person1 I am fine"
+echo "how is you preparation $person1"
+echo "My devops preparation is fine $person"
