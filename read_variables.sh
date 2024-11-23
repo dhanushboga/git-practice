1aa178a8b0d822fa7099dabeb92e4b1f777744b3
dhanushboga <dhanushboga555@gmail.com>
2024-11-23 18:30:14 +0530

    read_variables script

---
+++ read_variables.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo "enter the username"
+
+read -s USERNAME
+
+echo "entered user name is $USERNAME"
+
+echo "enter the password"
+
+read -s PASSWORD
+
+echo "entered password is $PASSWORD"
