26993de1a51809bcde56177eabd6fb6ab1aea1ed
dhanushboga <dhanushboga555@gmail.com>
2024-11-23 18:41:44 +0530

    Array Script

---
+++ array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "kiwi" "orange")
+
+echo "the first entered fruit is ${FRUITS[0]}"
+echo "the second entered fruit is ${FRUITS[1]}"
+echo "the third entered fruit is ${FRUITS[2]}"
+
+echo "the all fruits are ${FRUITS[@]}"
